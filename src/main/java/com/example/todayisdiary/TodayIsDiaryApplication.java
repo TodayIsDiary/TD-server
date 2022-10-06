e3e72cb9650b69bf134dd38c366fffca82279d5b
beargame123 <hoyoung7827@gmail.com>
2022-10-06 10:14:21 +0900

    ⚙️:: setting

---
+++ src/main/java/com/example/todayisdiary/TodayIsDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.todayisdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayIsDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayIsDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todayisdiary/TodayIsDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayisdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayIsDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
