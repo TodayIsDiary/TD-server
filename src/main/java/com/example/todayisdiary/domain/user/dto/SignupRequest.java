c8885ab3aa9a84e55e336b55ecf08f39055b2de3
beargame123 <hoyoung7827@gmail.com>
2022-10-11 23:46:14 +0900

    ➕:: 회원가입 request add

--- src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
@@ -1,4 +1,23 @@
 package com.example.todayisdiary.domain.user.dto;
 
+import com.example.todayisdiary.domain.user.enums.Sex;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class SignupRequest {
+    // 아이디
+    private String accountId;
+
+    // 닉네임
+    private String nickName;
+
+    private String email;
+
+    private String password;
+
+    private Sex sex;
+
+    private String introduction;
 }
