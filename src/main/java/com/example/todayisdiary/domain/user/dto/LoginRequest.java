b8dc9dba0eeb6fbb2260caf12d91ad2346c62c14
beargame123 <hoyoung7827@gmail.com>
2022-10-11 23:45:17 +0900

    ➕:: loginRequest add

--- src/main/java/com/example/todayisdiary/domain/user/dto/LoginRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.example.todayisdiary.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginRequest {
+    private String accountId;
+    private String password;
 }
