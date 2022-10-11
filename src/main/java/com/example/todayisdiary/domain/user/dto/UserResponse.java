0611c5b409d80171823e0eb0e959d96f6074960d
beargame123 <hoyoung7827@gmail.com>
2022-10-11 23:46:32 +0900

    ➕:: userResponse add

--- src/main/java/com/example/todayisdiary/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/UserResponse.java
@@ -1,4 +1,21 @@
 package com.example.todayisdiary.domain.user.dto;
 
+import com.example.todayisdiary.domain.user.entity.User;
+import com.example.todayisdiary.domain.user.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class UserResponse {
+    private final String email;
+    private final String accountId;
+    private final Role role;
+
+    public static UserResponse of(User user){
+            return UserResponse.builder()
+                    .accountId(user.getAccountId())
+                    .email(user.getEmail())
+                    .role(user.getRole()).build();
+    }
 }
