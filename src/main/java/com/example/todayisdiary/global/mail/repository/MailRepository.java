922af3da05d3def7516438e48d930688817e41f3
beargame123 <hoyoung7827@gmail.com>
2022-10-12 17:18:01 +0900

    ➕:: mailRepository add

---
+++ src/main/java/com/example/todayisdiary/global/mail/repository/MailRepository.java
@@ -0,0 +1,13 @@
+package com.example.todayisdiary.global.mail.repository;
+
+import com.example.todayisdiary.global.mail.entity.Mail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MailRepository extends JpaRepository<Mail, Long> {
+
+    Optional<Mail> findByCode(String code);
+}
