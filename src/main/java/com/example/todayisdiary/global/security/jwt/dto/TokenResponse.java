11d6716a7d5c61832453b93cbc34a84951fdbc54
beargame123 <hoyoung7827@gmail.com>
2022-10-12 17:29:42 +0900

    ➕:: response add

---
+++ src/main/java/com/example/todayisdiary/global/security/jwt/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.example.todayisdiary.global.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private final String atk;
+    private final String rtk;
+
+}
