9bf98d5d80ace9936f06581c9a1d4fce698964f6
beargame123 <hoyoung7827@gmail.com>
2022-10-16 01:11:23 +0900

    🛠️:: provider logic change

--- src/main/java/com/example/todayisdiary/global/security/jwt/JwtProvider.java
+++ src/main/java/com/example/todayisdiary/global/security/jwt/JwtProvider.java
@@ -1,8 +1,9 @@
 package com.example.todayisdiary.global.security.jwt;
 
 import com.example.todayisdiary.domain.user.dto.UserResponse;
-import com.example.todayisdiary.global.security.jwt.dao.RedisDao;
 import com.example.todayisdiary.global.security.jwt.dto.TokenResponse;
+import com.example.todayisdiary.global.security.jwt.entity.RefreshToken;
+import com.example.todayisdiary.global.security.jwt.repository.RefreshTokenRepository;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +16,8 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import java.time.Duration;
 import java.util.Base64;
 import java.util.Date;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
@@ -26,7 +25,7 @@ import java.util.Objects;
 public class JwtProvider {
 
     private final UserDetailsService userDetailsService;
-    private final RedisDao redisDao;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Value("${spring.jwt.key}")
     private String key;
@@ -38,22 +37,28 @@ public class JwtProvider {
     private Long rtkTime;
 
     @PostConstruct
-    protected void init(){key = Base64.getEncoder().encodeToString(key.getBytes());
+    protected void init() {
+        key = Base64.getEncoder().encodeToString(key.getBytes());
     }
 
-    public TokenResponse createTokenByLogin(UserResponse userResponse){
+    public TokenResponse createTokenByLogin(UserResponse userResponse) {
         String atk = createToken(userResponse, atkTime, "atk");
         String rtk = createToken(userResponse, rtkTime, "rtk");
-        redisDao.setValues(userResponse.getAccountId(), rtk, Duration.ofMillis(rtkTime));
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .accountId(userResponse.getAccountId())
+                        .refreshToken(rtk)
+                        .rtkTime(rtkTime).build()
+        );
         return new TokenResponse(atk, rtk);
     }
 
     // Jwt 토큰 인증 정보조회
-    public Authentication getAuthentication(String token){
+    public Authentication getAuthentication(String token) {
         // Jwt 에서 정보 추출
         Claims claims = parseClaims(token);
         // 권한 정보가 없음
-        if (claims.get("roles") == null){
+        if (claims.get("roles") == null) {
             log.error("YOUR_NOT_ROLE");
         }
         UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
@@ -61,37 +66,37 @@ public class JwtProvider {
     }
 
     // Jwt 토큰 복호화해서 가져오기
-    public Claims parseClaims(String token){
+    public Claims parseClaims(String token) {
         try {
             return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
-        }catch (ExpiredJwtException e){
+        } catch (ExpiredJwtException e) {
             return e.getClaims();
         }
     }
 
     // Request의 Header 에서 token값을 가져옴. "Authorization" : "TOKEN 값"
-    public String resolveToken(HttpServletRequest request){
+    public String resolveToken(HttpServletRequest request) {
         return request.getHeader("Authorization");
     }
 
     // 토큰의 유효성 + 만료일자 확인
-    public boolean validateToken(String jwtToken){
+    public boolean validateToken(String jwtToken) {
         try {
             Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
             return true;
-        }catch (JwtException | IllegalArgumentException e){
+        } catch (JwtException | IllegalArgumentException e) {
             log.error(e.toString());
             return false;
         }
     }
 
-    public TokenResponse reissueAtk(UserResponse userResponse){
-        String rtkInRedis = redisDao.getValues(userResponse.getAccountId());
-        if (Objects.isNull(rtkInRedis)) throw new IllegalStateException("인증 정보가 만료되었습니다.");
+    public TokenResponse reissueAtk(UserResponse userResponse) {
+        boolean rtkInRedis = refreshTokenRepository.existsByAccountId(userResponse.getAccountId());
+        if (rtkInRedis) throw new IllegalStateException("인증 정보가 만료되었습니다.");
         return new TokenResponse(createToken(userResponse, atkTime, "atk"), null);
     }
 
-    private String createToken(UserResponse userResponse, Long tokenTime, String type){
+    private String createToken(UserResponse userResponse, Long tokenTime, String type) {
         Claims claims = Jwts.claims().setSubject(userResponse.getAccountId());
         claims.put("roles", userResponse.getRole());
         Date date = new Date();
