a05ba059aef9f9041ed1401eea41f5f1d2561b22
beargame123 <hoyoung7827@gmail.com>
2022-10-16 01:04:49 +0900

    ➕:: swagger 관련 메소드 추가

--- src/main/java/com/example/todayisdiary/domain/user/controller/UserController.java
+++ src/main/java/com/example/todayisdiary/domain/user/controller/UserController.java
@@ -9,6 +9,8 @@ import com.example.todayisdiary.global.mail.dto.MailDto;
 import com.example.todayisdiary.global.security.auth.AuthDetails;
 import com.example.todayisdiary.global.security.jwt.JwtProvider;
 import com.example.todayisdiary.global.security.jwt.dto.TokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Tag(name = "user", description = "유저에 대한 API 입니다.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
@@ -24,10 +27,12 @@ public class UserController {
     private final UserService userService;
     public final JwtProvider jwtProvider;
 
+    @Operation(summary = "회원가입") 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponse signUp(@RequestBody SignupRequest request){ return userService.signup(request);}
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.CREATED)
     public TokenResponse login(@RequestBody LoginRequest request){
@@ -35,6 +40,7 @@ public class UserController {
         return jwtProvider.createTokenByLogin(userResponse);
     }
 
+    @Operation(summary = "토큰 재발급")
     @GetMapping("/reissue")
     public TokenResponse reissue(
             @AuthenticationPrincipal AuthDetails authDetails
@@ -43,11 +49,13 @@ public class UserController {
         return jwtProvider.reissueAtk(userResponse);
     }
 
+    @Operation(summary = "비밀번호 찾기-이메일 입력")
     @PostMapping("/lost/password")
     public void mail(@Valid @RequestBody MailDto dto){
         userService.lostPassword(dto);
     }
 
+    @Operation(summary = "비밀번호 찾기")
     @PatchMapping("/lost/password")
     public void setPassword(@Valid @RequestBody PasswordRequest request){
         userService.setPassword(request);
