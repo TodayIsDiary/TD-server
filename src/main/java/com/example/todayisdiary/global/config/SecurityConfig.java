98ce85d4e0c5f65b79ac1422d696ac3913af391c
beargame123 <hoyoung7827@gmail.com>
2022-10-20 23:45:09 +0900

    🛠️:: 설정변경

--- src/main/java/com/example/todayisdiary/global/config/SecurityConfig.java
+++ src/main/java/com/example/todayisdiary/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,49 +23,38 @@ public class SecurityConfig {
     private final JwtProvider jwtProvider;
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return (web) -> web.ignoring().antMatchers("/js/**", "/css/**", "/html/**");
-    }
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
 
-    @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-            http
-                    .rememberMe()
-                    .key("uniqueAndSecret")
-                    .rememberMeParameter("remember-me")
-                    .tokenValiditySeconds(86400 * 14)
+                .formLogin().disable()
+                .csrf().disable()
+                .cors().disable()
 
-                    .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                    //.csrf().disable()
-                    //.cors().disable()
+                .and()
 
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.POST,"/user/login").permitAll()
-                    .antMatchers(HttpMethod.POST,"/user/signup").permitAll()
-                    .antMatchers(HttpMethod.GET, "/user/reissue").permitAll()
-                    .antMatchers(HttpMethod.POST, "/user/lost/password").permitAll()
-                    .antMatchers(HttpMethod.PATCH,"/user/lost/password").permitAll()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/reissue").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/lost/password").permitAll()
+                .antMatchers(HttpMethod.PATCH, "/user/lost/password").permitAll()
 
-                    .antMatchers("/swagger-ui/**").permitAll()
-                    .antMatchers("/v3/api-docs/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/v3/api-docs/**").permitAll()
 
-                    .anyRequest().authenticated()
+                .anyRequest().authenticated()
 
-                    .and()
+                .and()
 
-                    .logout()
-                    .deleteCookies("remember-me")
-
-                    .and()
-
-                    .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
                         UsernamePasswordAuthenticationFilter.class);
-            return http.build();
+        return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
