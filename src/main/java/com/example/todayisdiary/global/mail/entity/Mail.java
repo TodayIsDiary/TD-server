f11353ea34b3fb2c7b20841d06379e6091e4cfbd
beargame123 <hoyoung7827@gmail.com>
2022-10-31 10:00:46 +0900

    ➕:: mail Entity add

---
+++ src/main/java/com/example/todayisdiary/global/mail/entity/Mail.java
@@ -0,0 +1,39 @@
+package com.example.todayisdiary.global.mail.entity;
+
+import com.example.todayisdiary.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Mail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+
+    @Builder
+    public Mail(String code, String email){
+        this.code = code;
+        this.email = email;
+    }
+
+    @Builder
+    public Mail(String code, String email, User user){
+        this.code = code;
+        this.email = email;
+        this.user = user;
+    }
+
+}
