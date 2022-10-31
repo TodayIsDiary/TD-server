56b717b7db9c6517bf46e11a0a1cb4e5118bf26b
beargame123 <hoyoung7827@gmail.com>
2022-10-31 11:38:33 +0900

    ➕:: annotation add

--- src/main/java/com/example/todayisdiary/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/todayisdiary/domain/user/repository/UserRepository.java
@@ -2,19 +2,18 @@ package com.example.todayisdiary.domain.user.repository;
 
 import com.example.todayisdiary.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByAccountId(String accountId);
 
-    Optional<User> findByAccountId(String accountId);
+    Optional<User> findUserByAccountId(String accountId);
 
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByCode(String code);
-
+    Optional<User> findUserByEmail(String email);
 }
