523d059df23c228ed8cd256e929333c739e3d7f9
beargame123 <hoyoung7827@gmail.com>
2022-11-07 23:44:49 +0900

    ➕:: reponse dto add

---
+++ src/main/java/com/example/todayisdiary/domain/board/dto/BoardResponse.java
@@ -0,0 +1,22 @@
+package com.example.todayisdiary.domain.board.dto;
+
+import com.example.todayisdiary.domain.board.enums.BoardCategory;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class BoardResponse {
+    private String title;
+
+    private String content;
+
+    private BoardCategory category;
+
+    private LocalDateTime boardTime;
+
+    private String writer;
+
+}
