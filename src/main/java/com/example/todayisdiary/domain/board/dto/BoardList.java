e1525cf2c2cc75286716e076f5db2928e96aad25
beargame123 <hoyoung7827@gmail.com>
2022-11-07 23:45:45 +0900

    ➕:: boardList add

---
+++ src/main/java/com/example/todayisdiary/domain/board/dto/BoardList.java
@@ -0,0 +1,14 @@
+package com.example.todayisdiary.domain.board.dto;
+
+import com.example.todayisdiary.domain.board.enums.BoardCategory;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class BoardList {
+    private String title;
+    private BoardCategory category;
+    private String date; // 몇분전 글인지 볼 수 있게 해준다. 예 : 방금전, 5분전
+
+}
