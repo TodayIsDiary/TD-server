e8767914559fcb81386974ccae4c124622ac42e5
beargame123 <hoyoung7827@gmail.com>
2022-11-07 23:02:00 +0900

    ➕:: userService add

--- src/main/java/com/example/todayisdiary/domain/user/service/UserService.java
+++ src/main/java/com/example/todayisdiary/domain/user/service/UserService.java
@@ -1,7 +1,6 @@
 package com.example.todayisdiary.domain.user.service;
 
 import com.example.todayisdiary.domain.user.dto.*;
-import com.example.todayisdiary.domain.user.entity.User;
 import com.example.todayisdiary.global.mail.dto.MailRequest;
 
 public interface UserService {
@@ -16,19 +15,19 @@ public interface UserService {
     void lostPassword(MailRequest mailDto) throws Exception;
 
     // 코드 인증후 비밀번호 변경, PATCH , /user/lost/password
-    void setPassword(PasswordRequest request);
+    void setPasswordEmail(PasswordRequest request);
 
     // 토큰 비밀번호 변경, PATCH, /user/password
-    void setPasswords(String accountId, PasswordRequest request);
+    void setPasswords(PasswordRequest request);
 
     // 내 정보 불러오기, GET, /user
-    User getUser(String accountId);
+    UserInfoResponse getUser();
 
     // 마이페이지 수정, PATCH, /user/change , 수정 되는것 : nickName, introduction
-    void setUser(String accountId, UserRequest request);
+    void setUser(UserRequest request);
 
     // 회원 탈퇴하기, DELETE, /user/leave
-    void leaveUser(String accountId);
+    void leaveUser();
 
     void signupEmail(MailRequest mailDto) throws Exception;
 }
