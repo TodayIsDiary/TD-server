2038f3f2beba50432f3455d4e49fe6078e70d885
beargame123 <hoyoung7827@gmail.com>
2022-11-07 23:45:21 +0900

    ➕:: request dto add

---
+++ src/main/java/com/example/todayisdiary/domain/board/dto/BoardRequest.java
@@ -0,0 +1,15 @@
+package com.example.todayisdiary.domain.board.dto;
+
+import com.example.todayisdiary.domain.board.enums.BoardCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BoardRequest {
+    private String title;
+
+    private String content;
+
+    private BoardCategory category;
+}
