f9f79e13f53e22e549a59efcb7e4f6fffdb2abae
beargame123 <hoyoung7827@gmail.com>
2022-11-07 23:42:19 +0900

    🛠️:: entity modify

--- src/main/java/com/example/todayisdiary/domain/board/entity/Board.java
+++ src/main/java/com/example/todayisdiary/domain/board/entity/Board.java
@@ -1,14 +1,16 @@
 package com.example.todayisdiary.domain.board.entity;
 
-import com.example.todayisdiary.domain.board.enums.ContentCategory;
+import com.example.todayisdiary.domain.board.enums.BoardCategory;
 import com.example.todayisdiary.domain.user.entity.User;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "board")
@@ -22,20 +24,29 @@ public class Board {
 
     private String content;
 
-    private ContentCategory category;
+    private BoardCategory category;
+
+    // 업데이트와 만들어진 시간
+    private LocalDateTime boardTime;
+
+    @PrePersist
+    public void prePersist(){
+        this.boardTime = LocalDateTime.now();
+    }
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Builder
-    public Board(String title, String content, ContentCategory category){
+    public Board(String title, String content, BoardCategory category,User user){
         this.title = title;
         this.content = content;
         this.category = category;
+        this.user = user;
     }
 
-    public void setBord(String title, String content, ContentCategory category){
+    public void setBord(String title, String content, BoardCategory category){
         this.title = title;
         this.content = content;
         this.category = category;
