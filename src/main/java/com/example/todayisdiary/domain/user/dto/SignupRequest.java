edc8f0473f35cd9dc68da15ee4606435725433e6
beargame123 <hoyoung7827@gmail.com>
2022-11-17 23:50:20 +0900

    ➕:: java 정규식 add

--- src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
@@ -4,6 +4,8 @@ import com.example.todayisdiary.domain.user.enums.Sex;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Pattern;
+
 @Getter
 @AllArgsConstructor
 public class SignupRequest {
@@ -15,6 +17,8 @@ public class SignupRequest {
 
     private String email;
 
+    @Pattern(regexp="^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*\\\\W).{8,20}$",
+            message = "비밀번호는 영문과 특수문자 숫자를 포함하여 8자 이상이어야 합니다.")
     private String password;
 
     // 메일 인증 코드
