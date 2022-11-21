8c3ccf02403946b015c00cc55cd20ab753305771
beargame123 <hoyoung7827@gmail.com>
2022-11-21 09:06:30 +0900

    ➕:: Pattern 적용

--- src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/SignupRequest.java
@@ -17,8 +17,8 @@ public class SignupRequest {
 
     private String email;
 
-    @Pattern(regexp="^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*\\\\W).{8,20}$",
-            message = "비밀번호는 영문과 특수문자 숫자를 포함하여 8자 이상이어야 합니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
     // 메일 인증 코드
