b0d9e6a51d508553b0bf1a3070f3cd39387e3389
beargame123 <hoyoung7827@gmail.com>
2022-11-21 09:56:31 +0900

    🛠️:: entity modify

--- src/main/java/com/example/todayisdiary/domain/Chat/entity/Chat.java
+++ src/main/java/com/example/todayisdiary/domain/Chat/entity/Chat.java
@@ -1,6 +1,8 @@
 package com.example.todayisdiary.domain.Chat.entity;
 
+import com.example.todayisdiary.domain.board.entity.Board;
 import com.example.todayisdiary.domain.user.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,11 +21,19 @@ public class Chat {
     private String comment;
 
     @ManyToOne
-    @JoinColumn(name = "user", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "user", nullable = false)
-    private User user;
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @Builder
+    public Chat(String writer, String comment, User user, Board board) {
+        this.writer = writer;
+        this.comment = comment;
+        this.user = user;
+        this.board = board;
+    }
 
 }
