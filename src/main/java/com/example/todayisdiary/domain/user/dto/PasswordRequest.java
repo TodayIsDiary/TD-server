c03f39fe3140bb78374331b199619d2e0eafc1ed
beargame123 <hoyoung7827@gmail.com>
2022-11-21 09:09:34 +0900

    🛠️:: 비밀번호 정규식 추가

--- src/main/java/com/example/todayisdiary/domain/user/dto/PasswordRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/PasswordRequest.java
@@ -3,11 +3,15 @@ package com.example.todayisdiary.domain.user.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor
 public class PasswordRequest {
     private String code;
     private String originalPassword;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
     private String newPasswordValid;
 }
