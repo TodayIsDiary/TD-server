7178c83e68b9ae1f17034133090930f042e33c5e
beargame123 <hoyoung7827@gmail.com>
2022-11-22 23:13:25 +0900

    ➕:: 댓글 Entity add

--- src/main/java/com/example/todayisdiary/domain/Chat/entity/Chat.java
+++ src/main/java/com/example/todayisdiary/domain/Chat/entity/Chat.java
@@ -2,14 +2,17 @@ package com.example.todayisdiary.domain.Chat.entity;
 
 import com.example.todayisdiary.domain.board.entity.Board;
 import com.example.todayisdiary.domain.user.entity.User;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Chat {
     @Id
@@ -20,6 +23,21 @@ public class Chat {
 
     private String comment;
 
+    private Long originChatId;
+
+    private Long replyChatId;
+
+    // 업데이트와 만들어진 시간
+    private LocalDateTime chatTime;
+
+    @ColumnDefault("false")
+    private boolean originChat;
+
+    @PrePersist
+    public void prePersist(){
+        this.chatTime = LocalDateTime.now();
+    }
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -28,12 +46,22 @@ public class Chat {
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
+    public void isOrigin(){
+        this.originChat = true;
+    }
+
     @Builder
-    public Chat(String writer, String comment, User user, Board board) {
+    public Chat(String writer, String comment, User user, Board board, Long originChatId, Long replyChatId) {
         this.writer = writer;
         this.comment = comment;
         this.user = user;
         this.board = board;
+        this.originChatId = originChatId;
+        this.replyChatId= replyChatId;
+    }
+
+    public void setChat(String comment){
+        this.comment = comment;
     }
 
 }
