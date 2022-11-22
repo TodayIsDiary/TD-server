04d518a20653c6f084d0ad8cc1c2e60c2655b3f4
beargame123 <hoyoung7827@gmail.com>
2022-11-22 23:15:32 +0900

    ➕:: chat Dto Request add

---
+++ src/main/java/com/example/todayisdiary/domain/Chat/dto/ChatRequest.java
@@ -0,0 +1,10 @@
+package com.example.todayisdiary.domain.Chat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatRequest {
+    private String comment;
+}
