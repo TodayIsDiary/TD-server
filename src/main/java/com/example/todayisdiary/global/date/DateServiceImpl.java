fb64fdba00591b53f08c19877a60657537a25b3a
beargame123 <hoyoung7827@gmail.com>
2022-11-22 23:07:29 +0900

    ➕:: betweenDate method add

---
+++ src/main/java/com/example/todayisdiary/global/date/DateServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.todayisdiary.global.date;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+
+@Service
+public class DateServiceImpl implements DateService {
+    @Override
+    public String betweenDate(LocalDateTime dateTime) {
+        LocalDateTime now = LocalDateTime.now();
+        Duration diff = Duration.between(dateTime.toLocalTime(), now.toLocalTime());
+        if (diff.getSeconds() >= 60) {
+            if (diff.toMinutes() >= 60) {
+                if (diff.toHours() >= 24) {
+                    Period period = Period.between(dateTime.toLocalDate(), now.toLocalDate());
+                    if (period.getDays() >= 30) {
+                        if (period.getMonths() >= 12) {
+                            return period.getYears() + "년전";
+                        } else return period.getMonths() + "달전";
+                    } else return period.getDays() + "일전";
+                } else return diff.toHours() + "시간전";
+            } else return diff.toMinutes() + "분전";
+        } else return "방금전";
+    }
+}
