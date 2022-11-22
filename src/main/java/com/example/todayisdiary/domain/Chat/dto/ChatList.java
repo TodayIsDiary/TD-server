c9bcf3809e2c277e4a315370ffd43bbe101f7f7f
beargame123 <hoyoung7827@gmail.com>
2022-11-22 23:15:49 +0900

    ➕:: chat Dto List

---
+++ src/main/java/com/example/todayisdiary/domain/Chat/dto/ChatList.java
@@ -0,0 +1,15 @@
+package com.example.todayisdiary.domain.Chat.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ChatList {
+    private final Long id;
+    private final String comment;
+    private final String writer;
+    private final String date;
+    private final Long originChatId;
+    private final Long replyChatId;
+}
