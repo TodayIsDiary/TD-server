2fa9955c176e9efc9b672a2e1ed9bdfc22af1bb7
beargame123 <hoyoung7827@gmail.com>
2022-11-28 15:47:14 +0900

    🛠️:: AllById -> OriginChatId change

--- src/main/java/com/example/todayisdiary/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/todayisdiary/domain/comment/repository/CommentRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findChatById(Long id);
 
-    List<Comment> findAllById(Long id);
+    List<Comment> findCommentByOriginChatId(Long id);
 }
