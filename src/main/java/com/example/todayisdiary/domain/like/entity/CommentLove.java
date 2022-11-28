feb84c6f3eac6dd3af2f3275227b637fe706c37e
beargame123 <hoyoung7827@gmail.com>
2022-11-28 10:14:25 +0900

    ➕:: commentLove Entity add

---
+++ src/main/java/com/example/todayisdiary/domain/like/entity/CommentLove.java
@@ -0,0 +1,33 @@
+package com.example.todayisdiary.domain.like.entity;
+
+import com.example.todayisdiary.domain.comment.entity.Comment;
+import com.example.todayisdiary.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLove {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Builder
+    public CommentLove(User user, Comment comment){
+        this.user = user;
+        this.comment = comment;
+    }
+}
