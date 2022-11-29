7ff5a1137d340d31a4782fb9c4ef1f74413d451e
beargame123 <hoyoung7827@gmail.com>
2022-11-29 09:58:53 +0900

    ➕:: CommentReportList dto add

---
+++ src/main/java/com/example/todayisdiary/domain/report/dto/CommentReportList.java
@@ -0,0 +1,13 @@
+package com.example.todayisdiary.domain.report.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CommentReportList {
+    private Long reportId;
+    private String reporter;
+    private String title;
+    private Long commentId;
+}
