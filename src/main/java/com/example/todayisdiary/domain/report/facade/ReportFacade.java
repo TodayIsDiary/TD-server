ff47c67c62bd3871f1b89e49e0e4916444c86ae5
beargame123 <hoyoung7827@gmail.com>
2022-11-29 19:04:06 +0900

    🛠️:: comment method add

--- src/main/java/com/example/todayisdiary/domain/report/facade/ReportFacade.java
+++ src/main/java/com/example/todayisdiary/domain/report/facade/ReportFacade.java
@@ -1,6 +1,8 @@
 package com.example.todayisdiary.domain.report.facade;
 
+import com.example.todayisdiary.domain.report.entity.CommentReport;
 import com.example.todayisdiary.domain.report.entity.Report;
+import com.example.todayisdiary.domain.report.repository.CommentReportRepository;
 import com.example.todayisdiary.domain.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -12,10 +14,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReportFacade {
     private final ReportRepository reportRepository;
+    private final CommentReportRepository commentReportRepository;
 
     public Report getReportById(Long id) {
         return reportRepository.findReportById(id)
                 .orElseThrow(() -> new IllegalArgumentException("신고내용을 찾을 수 없습니다."));
+    }
+
+    public CommentReport getCommentReportById(Long id) {
+        return commentReportRepository.findCommentReportById(id)
+                .orElseThrow(() -> new IllegalArgumentException("신고내용을 찾을 수 없습니다."));
+    }
+
+    public List<CommentReport> getCommentAllById(Sort sort){
+        return commentReportRepository.findAll(sort);
     }
 
     public List<Report> getBoardAllById(Sort sort){
