623c9b66ec5a8f61b4066c1964facc3ee4eda747
beargame123 <hoyoung7827@gmail.com>
2022-11-29 19:08:35 +0900

    ➕:: 신고 댓글 method add

--- src/main/java/com/example/todayisdiary/domain/report/service/ReportService.java
+++ src/main/java/com/example/todayisdiary/domain/report/service/ReportService.java
@@ -1,19 +1,25 @@
 package com.example.todayisdiary.domain.report.service;
 
-import com.example.todayisdiary.domain.report.dto.ReportList;
-import com.example.todayisdiary.domain.report.dto.ReportRequest;
-import com.example.todayisdiary.domain.report.dto.ReportResponse;
-
-import java.util.List;
+import com.example.todayisdiary.domain.report.dto.*;
 
 public interface ReportService {
     void createReport(ReportRequest request, Long id);
 
+    void createCommentReport(ReportRequest request, Long id);
+
     void delReport(Long id);
 
-    List<ReportList> reportBoardList();
+    void delCommentReport(Long id);
+
+    ResponseBoardList reportBoardList();
+
+    ResponseCommentList reportCommentList();
 
     ReportResponse detailReport(Long id);
 
+    CommentReportResponse detailCommentReport(Long id);
+
     void forceDelBoard(Long id);
+
+    void forceDelComment(Long id);
 }
