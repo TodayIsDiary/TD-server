0427d221074463620189b0fecd1ea4f6f2f5c99f
beargame123 <hoyoung7827@gmail.com>
2022-12-02 14:05:05 +0900

    ➕:: userReport service add

--- src/main/java/com/example/todayisdiary/domain/report/service/ReportService.java
+++ src/main/java/com/example/todayisdiary/domain/report/service/ReportService.java
@@ -1,25 +1,42 @@
 package com.example.todayisdiary.domain.report.service;
 
 import com.example.todayisdiary.domain.report.dto.*;
+import com.example.todayisdiary.domain.report.dto.request.ReportRequest;
 
 public interface ReportService {
     void createReport(ReportRequest request, Long id);
 
     void createCommentReport(ReportRequest request, Long id);
 
+    void createUserReport(ReportRequest request, Long id);
+
     void delReport(Long id);
 
     void delCommentReport(Long id);
 
-    ResponseBoardList reportBoardList();
+    void delUserReport(Long id);
 
-    ResponseCommentList reportCommentList();
+    ReportListResponse reportBoardList();
+
+    ReportListResponse userReportList();
+
+    ReportListResponse reportBoardFilterList(String title);
+
+    ReportListResponse reportCommentList();
+
+    ReportListResponse reportCommentFilterList(String title);
+
+    ReportListResponse userReportFilterList(String title);
 
     ReportResponse detailReport(Long id);
 
     CommentReportResponse detailCommentReport(Long id);
 
+    UserReportResponse detailUserReport(Long id);
+
     void forceDelBoard(Long id);
 
     void forceDelComment(Long id);
+
+    void forceDelUser(Long id);
 }
