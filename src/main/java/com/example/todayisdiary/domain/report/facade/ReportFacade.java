aa59bd54ad4baf6158ffbe3d508f20169aefd6c4
beargame123 <hoyoung7827@gmail.com>
2022-12-02 14:07:22 +0900

    ➕:: userReport Facade add

--- src/main/java/com/example/todayisdiary/domain/report/facade/ReportFacade.java
+++ src/main/java/com/example/todayisdiary/domain/report/facade/ReportFacade.java
@@ -2,8 +2,10 @@ package com.example.todayisdiary.domain.report.facade;
 
 import com.example.todayisdiary.domain.report.entity.CommentReport;
 import com.example.todayisdiary.domain.report.entity.Report;
+import com.example.todayisdiary.domain.report.entity.UserReport;
 import com.example.todayisdiary.domain.report.repository.CommentReportRepository;
 import com.example.todayisdiary.domain.report.repository.ReportRepository;
+import com.example.todayisdiary.domain.report.repository.UserReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
@@ -15,15 +17,34 @@ import java.util.List;
 public class ReportFacade {
     private final ReportRepository reportRepository;
     private final CommentReportRepository commentReportRepository;
+    private final UserReportRepository userReportRepository;
 
     public Report getReportById(Long id) {
         return reportRepository.findReportById(id)
-                .orElseThrow(() -> new IllegalArgumentException("신고내용을 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("신고된 일기를 찾을 수 없습니다."));
     }
 
     public CommentReport getCommentReportById(Long id) {
         return commentReportRepository.findCommentReportById(id)
-                .orElseThrow(() -> new IllegalArgumentException("신고내용을 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("신고된 댓글을 찾을 수 없습니다."));
+    }
+
+    public UserReport getUserReportById(Long id){
+        return userReportRepository.findUserReportById(id)
+                .orElseThrow(() -> new IllegalArgumentException("신고된 유저를 찾을 수 없습니다."));
+
+    }
+
+    public List<Report> getReportByTitle(String title){
+        return reportRepository.findAllReportByTitleSearch(title);
+    }
+
+    public List<CommentReport> getCommentReportByTitle(String title){
+        return commentReportRepository.findAllCommentReportByTitleSearch(title);
+    }
+
+    public List<UserReport> getUserReportByTitle(String title){
+        return userReportRepository.findUserReportsByTitleSearch(title);
     }
 
     public List<CommentReport> getCommentAllById(Sort sort){
@@ -33,4 +54,6 @@ public class ReportFacade {
     public List<Report> getBoardAllById(Sort sort){
         return reportRepository.findAll(sort);
     }
+
+    public List<UserReport> getUserAllById(Sort sort){return userReportRepository.findAll(sort);}
 }
