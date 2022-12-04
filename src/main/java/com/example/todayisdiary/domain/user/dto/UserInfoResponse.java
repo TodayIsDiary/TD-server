2794f0b6886f54298e5dcb28a1d19a21d5632094
beargame123 <hoyoung7827@gmail.com>
2022-12-04 23:48:21 +0900

    ➕:: myPage add

--- src/main/java/com/example/todayisdiary/domain/user/dto/UserInfoResponse.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/UserInfoResponse.java
@@ -1,5 +1,6 @@
 package com.example.todayisdiary.domain.user.dto;
 
+import com.example.todayisdiary.domain.user.enums.Sex;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,5 +9,9 @@ import lombok.Getter;
 public class UserInfoResponse {
     private final String nickName;
     private final String introduction;
-
+    private final int visit;
+    private final int love;
+    private final int comment;
+    private final Sex sex;
+    private final String email;
 }
