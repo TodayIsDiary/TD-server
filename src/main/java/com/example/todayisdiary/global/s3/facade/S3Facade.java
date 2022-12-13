1386664bcb12651c2c98d0201ebf561198762b71
beargame123 <hoyoung7827@gmail.com>
2022-12-13 14:08:19 +0900

    ➕:: s3 image delete method add

--- src/main/java/com/example/todayisdiary/global/s3/facade/S3Facade.java
+++ src/main/java/com/example/todayisdiary/global/s3/facade/S3Facade.java
@@ -1,5 +1,7 @@
 package com.example.todayisdiary.global.s3.facade;
 
+import com.example.todayisdiary.domain.board.entity.Board;
+import com.example.todayisdiary.domain.user.entity.User;
 import com.example.todayisdiary.global.s3.S3Upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,11 +20,15 @@ public class S3Facade {
         s3Upload.updateUser(multipartFile);
     }
 
-    public String getUrl(String s3FileName){
-        return s3Upload.getImageUrl(s3FileName);
-    }
-
     public void updateBoard(MultipartFile multipartFile, Long id){
         s3Upload.updateBoard(multipartFile, id);
+    }
+
+    public void delUser(User user){s3Upload.delUser(user);}
+
+    public void delBoard(Board board){s3Upload.delBoard(board);}
+
+    public String getUrl(String s3FileName){
+        return s3Upload.getImageUrl(s3FileName);
     }
 }
