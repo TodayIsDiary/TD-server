3bca0b334e9813b62d8accaf724c06c5d73364e6
beargame123 <hoyoung7827@gmail.com>
2023-02-03 16:03:36 +0900

    🛠️:: IllgalStateException -> Custom Refactoring

--- src/main/java/com/example/todayisdiary/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/todayisdiary/domain/user/facade/UserFacade.java
@@ -2,6 +2,8 @@ package com.example.todayisdiary.domain.user.facade;
 
 import com.example.todayisdiary.domain.user.entity.User;
 import com.example.todayisdiary.domain.user.repository.UserRepository;
+import com.example.todayisdiary.global.error.ErrorCode;
+import com.example.todayisdiary.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,17 +21,17 @@ public class UserFacade {
 
     public User getUserById(Long id){
         return userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
     }
 
     public User getUserByAccountId(String accountId) {
         return userRepository.findUserByAccountId(accountId)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     public User getUserByEmail(String email) {
         return userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("이메일을 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.EMAIL_NOT_FOUND));
     }
 }
