8b107fd29cf7a0fed7d3cfc61128406e1cc3ee9c
beargame123 <hoyoung7827@gmail.com>
2023-02-03 16:03:24 +0900

    🛠️:: IllgalStateException -> Custom Refactoring

--- src/main/java/com/example/todayisdiary/domain/comment/facade/CommentFacade.java
+++ src/main/java/com/example/todayisdiary/domain/comment/facade/CommentFacade.java
@@ -2,6 +2,8 @@ package com.example.todayisdiary.domain.comment.facade;
 
 import com.example.todayisdiary.domain.comment.entity.Comment;
 import com.example.todayisdiary.domain.comment.repository.CommentRepository;
+import com.example.todayisdiary.global.error.ErrorCode;
+import com.example.todayisdiary.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,7 @@ public class CommentFacade {
 
     public Comment getChatById(Long id) {
         return commentRepository.findChatById(id)
-                .orElseThrow(() -> new IllegalArgumentException("채팅을 찾지 못하였습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
     }
 
     public List<Comment> getChatByOriginId(Long id) {
