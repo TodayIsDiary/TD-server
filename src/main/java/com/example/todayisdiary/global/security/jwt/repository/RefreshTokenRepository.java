80ca9e2e5e92d3a3f505e680fbc378855ddc02e4
beargame123 <hoyoung7827@gmail.com>
2023-03-03 19:17:26 +0900

    🛠️:: String -> Long change

--- src/main/java/com/example/todayisdiary/global/security/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/todayisdiary/global/security/jwt/repository/RefreshTokenRepository.java
@@ -3,6 +3,6 @@ package com.example.todayisdiary.global.security.jwt.repository;
 import com.example.todayisdiary.global.security.jwt.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     boolean existsByAccountId(String accountId);
 }
