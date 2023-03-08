31f44c89c58ed4cf3d069e3c0bb7f6bda05006d1
beargame123 <hoyoung7827@gmail.com>
2023-03-08 22:46:59 +0900

    🛠️:: email parmether add

--- src/main/java/com/example/todayisdiary/domain/user/dto/PasswordRequest.java
+++ src/main/java/com/example/todayisdiary/domain/user/dto/PasswordRequest.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Pattern;
 @Getter
 @NoArgsConstructor
 public class PasswordRequest {
-    private String code;
+    private String email;
     private String originalPassword;
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
             message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
