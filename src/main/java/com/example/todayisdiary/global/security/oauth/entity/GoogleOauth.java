46a45fdf8b0cb57bdd0c29b19921e079576fc004
beargame123 <hoyoung7827@gmail.com>
2023-03-21 20:12:59 +0900

    🛠️:: RestTemplate() -> WebClient Refactoring

--- src/main/java/com/example/todayisdiary/global/security/oauth/entity/GoogleOauth.java
+++ src/main/java/com/example/todayisdiary/global/security/oauth/entity/GoogleOauth.java
@@ -8,8 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +28,7 @@ public class GoogleOauth {
     private final String google_userInfo_request_url = "https://www.googleapis.com/oauth2/v1/userinfo";
     private final ObjectMapper objectMapper;
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Value("${spring.security.oauth2.client.registration.google.clientId}")
     private String googleClientId;
@@ -41,43 +46,47 @@ public class GoogleOauth {
     }
 
     // 일회용 코드를 보내서 토큰을 포함한 Json이 담긴 ResponseEntity를 받아옴
-    public ResponseEntity<String> requestAccessToken(String code) {
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> params = new HashMap<>();
-        params.put("code", code);
-        params.put("client_id", googleClientId);
-        params.put("client_secret", googleSecret);
-        params.put("redirect_uri", googleRedirectUrl);
-        params.put("grant_type", "authorization_code");
+    public Mono<ResponseEntity<String>> requestAccessToken(String code) {
+        WebClient webClient = WebClient.builder().build();
 
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(google_token_request_url,
-                params, String.class);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("client_id", googleClientId);
+        params.add("client_secret", googleSecret);
+        params.add("redirect_uri", googleRedirectUrl);
+        params.add("grant_type", "authorization_code");
 
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            return responseEntity;
-        }
-        return null;
+        return webClient.post()
+                .uri(google_token_request_url)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(params))
+                .retrieve()
+                .toEntity(String.class)
+                .flatMap(responseEntity -> {
+                    if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                        return Mono.just(responseEntity);
+                    }
+                    return Mono.empty();
+                });
     }
 
-    public GoogleOAuthTokenDto getAccessToken(ResponseEntity<String> response) throws JsonProcessingException {
-        GoogleOAuthTokenDto googleOAuthTokenDto = objectMapper.readValue(response.getBody(), GoogleOAuthTokenDto.class);
+    public GoogleOAuthTokenDto getAccessToken(Mono<ResponseEntity<String>> response) throws JsonProcessingException {
+        GoogleOAuthTokenDto googleOAuthTokenDto = objectMapper.readValue(response.toString(), GoogleOAuthTokenDto.class);
         return googleOAuthTokenDto;
     }
 
-    public ResponseEntity<String> requestUserInfo(GoogleOAuthTokenDto oAuthToken) {
+    public Flux<String> requestUserInfo(GoogleOAuthTokenDto oAuthToken) {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer" + oAuthToken.getAccess_token());
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(google_userInfo_request_url, HttpMethod.GET, request, String.class);
-        return response;
+        return webClient.get()
+                .uri(google_userInfo_request_url)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer" + oAuthToken.getAccess_token())
+                .retrieve() // body를 받아 디코딩하는 간단한 메소드
+                .bodyToFlux(String.class);
     }
 
-    public GoogleUserInfoDto getUserInfo(ResponseEntity<String> response) throws JsonProcessingException {
+    public GoogleUserInfoDto getUserInfo(Flux<String> response) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        GoogleUserInfoDto googleUserInfoDto = objectMapper.readValue(response.getBody(), GoogleUserInfoDto.class);
-        return googleUserInfoDto;
+        return objectMapper.readValue(response.toString() , GoogleUserInfoDto.class);
     }
 
 }
